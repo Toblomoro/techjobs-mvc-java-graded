1674eba527ad4432ef1f153685a9c23c4350e4e1
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-10-19 13:56:24 -0500

    think i did it!

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -30,22 +31,21 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
-    @PostMapping(value = "results")
-    public String displaySearchResults(Model model, String searchType, String searchTerm) {
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam(required = false) String searchTerm) {
         ArrayList<Job> jobs;
-        if (searchType.equals("all"))
-        {
+        if (searchType.equals("all") && searchTerm.equals("all")) {
             jobs = JobData.findAll();
-            model.addAttribute("resultsHeader", "Search results:");
-            return "search";
-
+            model.addAttribute("title", "All Jobs");
         } else {
-            jobs= JobData.findByColumnAndValue(searchType,searchTerm);
-            model.addAttribute("resultsHeader", "Search results:");
-            model.addAttribute("jobs", jobs);
-            model.addAttribute("columns", columnChoices);
-            return "search";
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs containing " + columnChoices.get(searchType) + ": " + searchTerm);
         }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("lastSearchType", searchType);
+        return "search";
+
 
     };
 }
